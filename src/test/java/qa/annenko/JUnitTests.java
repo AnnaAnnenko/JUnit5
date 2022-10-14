d5e5316a2443e8e3f7d3b5c728ce7cd9591becdf
User <annenko.anya@gmail.com>
2022-10-14 09:51:42 +0300

    first params test

--- src/test/java/qa/annenko/JUnitTests.java
+++ src/test/java/qa/annenko/JUnitTests.java
@@ -1,5 +1,6 @@
 package qa.annenko;
 
+import com.codeborne.selenide.CollectionCondition;
 import org.assertj.core.error.ShouldHave;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,6 +8,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
 import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
@@ -14,24 +16,22 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class JUnitTests {
 
-//    static Stream<Arguments> nameOfTest() {
-//        return  Stream.of(
-//                Arguments.of("Аргумент1", "Аргумент2"),
-//                Arguments.of("Аргумент1", "Аргумент2")
-//        );
-//    }
-
+    static Stream<Arguments> nameOfTest() {
+        return  Stream.of(
+                Arguments.of(2, "Минск Гомель Брест"),
+                Arguments.of(1, "Москва Екатеринбург Санкт-Петербург Новосибирск Казань")
+                );
+    }
 //    @ValueSource(strings = {})
-//    @MethodSource("nameOfTest")
 
-//    @ParameterizedTest(name = "Проверка для элемента")
-   @Test
-    void nameOfTest() {
+    @MethodSource("nameOfTest")
+    @ParameterizedTest(name = "Проверка для элемента")
+    void nameOfTest(int numberInList, String cityName) {
         open("https://goldapple.ru/");
         $(".header-city__i").click();
         $(".select-field__view-button").click();
-        $(".select-field__option:first-child").click();
-        $(".city-selector__list")
-                .shouldHave(text("Москва"));
+        $(".select-field__option:nth-child(" + numberInList + ")").click();
+        $$(".city-selector__list")
+                .shouldHave(CollectionCondition.texts(cityName));
     }
 }
